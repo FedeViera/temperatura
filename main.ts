7169ad6ede8520176065944ae0608cb383d75298
Federico Viera <110137888+FedeViera@users.noreply.github.com>
2022-07-27 20:17:45 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+    basic.pause(100)
+    if (input.temperature() < 10) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(100)
+    }
+})
